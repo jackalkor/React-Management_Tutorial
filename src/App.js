eebb710e2e8e5cbf493c9f1c50c40b7390b5ac6a
jackalkor <jackalkor@gmail.com>
2023-01-30 11:04:16 +0900

    Apply material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+
+ const styles = theme => ({
+    root: {
+      width: '100%',
+      marginTop: theme.spacing.unit * 3, 
+      overflowX: "auto"
+    },
+    table: {
+      minWidth: 1080
+    }
+})
 
 const customers = [
    {
@@ -31,49 +50,31 @@ const customers = [
 
 class App extends Component {
     render() {
+      const { classes } = this.props;
       return (
-        <div>
-            {
-              customers.map(c => {
-                return (
-                    <Customer
-                      key={c.id}
-                      id={c.id}
-                      image={c.image}
-                      name={c.name}
-                      birthday={c.birthday}
-                      gender={c.gender}
-                      job={c.job}
-                    />
-                );
-              })
-            /* <Customer 
-              id={customers[0].id}
-              image={customers[0].image}
-              name={customers[0].name}
-              birthday={customers[0].birthday}
-              gender={customers[0].gender}
-              job={customers[0].job}
-            />
-            <Customer 
-              id={customers[1].id}
-              image={customers[1].image}
-              name={customers[1].name}
-              birthday={customers[1].birthday}
-              gender={customers[1].gender}
-              job={customers[1].job}
-            />
-            <Customer 
-              id={customers[2].id}
-              image={customers[2].image}
-              name={customers[2].name}
-              birthday={customers[2].birthday}
-              gender={customers[2].gender}
-              job={customers[2].job}
-            /> */
-            }
-        </div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>번호</TableCell>
+                  <TableCell>이미지</TableCell>
+                  <TableCell>이름</TableCell>
+                  <TableCell>생년월일</TableCell>
+                  <TableCell>성별</TableCell>
+                  <TableCell>직업</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                { customers.map(c => { return ( 
+                      <Customer key={c.id} id={c.id} image={c.image}  name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+                    );
+                  })
+                }
+              </TableBody>
+          </Table>
+        </Paper>
       );
     }
 }
-export default App;
+
+export default withStyles(styles)(App);
