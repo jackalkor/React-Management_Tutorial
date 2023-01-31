be82bdd59377e21563d78fde930c484bea651aaf
jackalkor <jackalkor@gmail.com>
2023-01-31 17:24:48 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -9,46 +9,31 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 
-
- const styles = theme => ({
+const styles = theme => ({
     root: {
       width: '100%',
-      marginTop: theme.spacing.unit * 3, 
+      marginTop: theme.spacing(3), 
       overflowX: "auto"
     },
     table: {
       minWidth: 1080
     }
-})
-
-const customers = [
-   {
-  'id':1,
-  'image':'https://placeimg.com/64/64/1',
-  'name':'홍길동',
-  'birthday':'961212',
-  'gender':'남자',
-  'job':'대학생'
-  },
-  {
-  'id':2,
-  'image':'https://placeimg.com/64/64/2',
-  'name':'임꺽정',
-  'birthday':'961023',
-  'gender':'남자',
-  'job':'대학생'
-  },
-  {
-  'id':3,
-  'image':'https://placeimg.com/64/64/3',
-  'name':'이순신',
-  'birthday':'960623',
-  'gender':'남자',
-  'job':'대학생'
-  }
-]
+});
 
 class App extends Component {
+    state ={
+      customers:''
+    }
+    componentDidMount(){
+      this.callApi()
+        .then(res => this.setState({customers: res}))
+        .catch(err => console.log(err));
+    }
+    callApi = async () => {
+      const response = await fetch('/api/customers');
+      const body = await response.json();
+      return body;
+    }
     render() {
       const { classes } = this.props;
       return (
@@ -65,11 +50,10 @@ class App extends Component {
                 </TableRow>
               </TableHead>
               <TableBody>
-                { customers.map(c => { return ( 
+                {this.state.customers ? this.state.customers.map(c => { return ( 
                       <Customer key={c.id} id={c.id} image={c.image}  name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
                     );
-                  })
-                }
+                  }) : ''}
               </TableBody>
           </Table>
         </Paper>
